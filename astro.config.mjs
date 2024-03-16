bc76cd1daa67a885f3cda26a739f3046ced03123
liu hongzhuang <691381755@qq.com>
2024-03-17 00:52:45 +0800

    ✨ feat: add Footer and Navigation components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), preact()]
+});
